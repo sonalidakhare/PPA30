b0f8e764e32d742465ba4264025fc154cc05c240
sonalidakhare <102373503+sonalidakhare@users.noreply.github.com>
2022-05-21 08:30:19 +0530

    Add files via upload

---
+++ Exception1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Exception1
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj= new Scanner(System.in);
+		
+		System.out.println("Please enter first number:");
+		int no1= sobj.nextInt();
+		
+		System.out.println("Please enter secong number:");
+		int no2= sobj.nextInt();
+		
+		int ans= no1/no2;
+		
+		System.out.println("Division is:"+ans);
+		sobj.close();
+		
+	}
+}
+		
